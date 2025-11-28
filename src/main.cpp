53d5cef04cb8fab0dfe4157a432ff309479d3ba9
Wang <>
2025-11-28 17:33:29 +0800

    src: Change the compiler option using-pdi to using-yaml to support Boost I/O.

--- src/main.cpp
+++ src/main.cpp
@@ -4,53 +4,62 @@
 
 #ifdef USE_BOOST
 #include <boost/asio.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
 #endif
 
-#ifdef USE_PDI
+#ifdef USE_YAML
 #include "pdi.hpp"
 #endif
 
 #define BUSNAME "xyz.openbmc_project.sdbusplus.example"
-#define Interface "/xyz/openbmc_project/sdbusplus"
+#define ObjPath "/xyz/openbmc_project/sdbusplus"
+#define Interface "xyz.openbmc_project.sdbusplus.example"
 
 int main()
 {
     /* code */
-    sdbusplus::bus::bus bus = sdbusplus::bus::new_default();
-    // int counter = 0;
 #ifdef USE_BOOST
     boost::asio::io_context io;
-    auto systemBus = std::make_shared<sdbusplus::asio::connection>(io, bus);
+    int value = 0;
+    auto systemBus = std::make_shared<sdbusplus::asio::connection>(io);
+    systemBus->request_name(BUSNAME);
     sdbusplus::asio::object_server objServer(systemBus);
     std::shared_ptr<sdbusplus::asio::dbus_interface> sdbusplusIface =
-        objServer.add_interface(Interface, BUSNAME);
+        objServer.add_interface(ObjPath, Interface);
     
     // Register methods and properties
-    sdbusplusIface->register_method("Get", [&]() { 
-        return counter; 
+    sdbusplusIface->register_method("Get", [value]() { 
+        return value; 
     });
 
-    sdbusplusIface->register_method("Set", [&](const int value) {
-        counter = value;
+    sdbusplusIface->register_method("Set", [&](const int newValue) {
+        value = newValue;
         return; 
     });
 
-    sdbusplus->register_property("Counter", counter);
+    sdbusplusIface->register_property("value", value, 
+        [](const int newValue,int& oldValue) {
+            oldValue = newValue + 1;
+            return true; 
+        },
+        [](int value) {
+            return value;
+        }
+    );
 
-    iface->initialize();
+    sdbusplusIface->initialize();
+    
+    io.run();
 #endif
 
-#ifdef USE_PDI
+#ifdef USE_YAML
+    sdbusplus::bus::bus bus = sdbusplus::bus::new_default();
     sdbusplus::server::manager_t objManager(bus, Interface);
     phosphor::pdi pdiObj(bus, Interface);
-#endif
-
     bus.request_name(BUSNAME);
-
-#ifdef USE_BOOST
-    io.run();
-#else
     bus.process_loop();
 #endif
+
     return 0;
 }
